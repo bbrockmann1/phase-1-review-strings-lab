11c99707cae65e614b43da41cbe6c7210b2b4fb5
Robert Brockmann <bbrockmann1@gmail.com>
2022-08-28 21:50:12 -0600

    commit tree 0745d788be460d921930407acecf3eabeb10c712

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Bob'
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+const shortGreeting = 'Welcome, ' + firstInitial + + restOfName + '!';
